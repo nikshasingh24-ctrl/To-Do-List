32f6a4b6e55c62dc3f8c94adf8763c5365a9a0aa
depressed_rat <nikshasingh24@gmail.com>
2025-09-02 19:06:13 +0530

    fixed a syntax error

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 function add(){
   let input = document.getElementById("task");
   work = input.value;
-  if(work === ""){alert = "enter a task"};
+  if(work === ""){alert(enter a task")};
   let item = document.createElement("li");
   item.textContent = "work";
   document.getElementById("todo").appendChild("item");
