baad3e3511bd4c1cf260587edce8a91d23b196b7
depressed_rat <nikshasingh24@gmail.com>
2025-09-02 19:02:08 +0530

    defined  a function add to add tasks

--- function.js
+++ function.js
@@ -1,1 +1,9 @@
-
+function add(){
+  let input = document.getElementById("task");
+  work = input.value;
+  if(work === ""){alert = "enter a task"};
+  let item = document.createElement("li");
+  item.textContent = "work";
+  document.getElementById("todo").appendChild("item");
+}
+  
